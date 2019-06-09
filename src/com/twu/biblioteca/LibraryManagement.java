ff73a690fcb4016b8081bd1c518899e9ff564131
Tom Shannon <tshannon@carthage.edu>
2019-06-09 09:29:14 -0500

    story 1.4: view menu options

--- src/com/twu/biblioteca/LibraryManagement.java
+++ src/com/twu/biblioteca/LibraryManagement.java
@@ -5,5 +5,15 @@ public class LibraryManagement
     public LibraryManagement()
     {
         System.out.println("Welcome to the Biblioteca Library Management System!");
+        viewMenuOptions();
+    }
+
+    public void viewMenuOptions()
+    {
+        System.out.println("Choose an option below to get started.\n");
+        System.out.println("1. View list of books");
+        System.out.println("2. Check out a book");
+        System.out.println("3. Return a book");
+        System.out.println("4. Quit the application");
     }
 }
