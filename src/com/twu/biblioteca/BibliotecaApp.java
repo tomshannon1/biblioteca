c93585f9a062810c63c760d363f6a30b3410e37e
Tom Shannon <tshannon@carthage.edu>
2019-06-09 09:24:20 -0500

    story 1.1: user sees a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
-public class BibliotecaApp {
+public class BibliotecaApp 
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) 
+    {
+        LibraryManagement biblioteca = new LibraryManagement();
     }
 }
---
+++ src/com/twu/biblioteca/LibraryManagement.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+public class LibraryManagement
+{
+    public LibraryManagement()
+    {
+        System.out.println("Welcome to the Biblioteca Library Management System!");
+    }
+}
